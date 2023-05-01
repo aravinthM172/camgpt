41152ebc11599c8e5b4f49a05d221f31d7b5a63d
aravinthM172 <112798331+aravinthM172@users.noreply.github.com>
2023-05-01 15:10:25 +0530

    Update main.py

--- camtxt/main.py
+++ camtxt/main.py
@@ -1,3 +1,22 @@
 import cv2
-from  PIL import Image
+import PIL import Image
 from pytesseract import pytesseract
+
+camera=cv2.VideoCapture(0)
+while True:
+    -,image=camera.read()
+    cv2.imshow('Text Detect',image)
+    if cv2.waitkey(1)& 0xFF==ord('s'):
+        cv2.imwrite('test1.jpg',image)
+        break
+camera.release()
+cv2.destroyAllWindow()
+def tesseract():
+    path_to_tesseract=r"C:\Users\placement\Downloads\tesseract-ocr-w64-setup-5.3.1.20230401.exe"
+    Imagepath='test1.png'
+    pytesseract.tesseract_cmd=path_to_tesseract
+    text=pytesseract.image_to_string(Image.open(Imagepath))
+    print(text[:-1])
+    tesseract()
+
+
